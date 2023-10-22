c528dd5d26ed46bcb2a1427dc0333543aaa38ee5
adithya61 <zonic662@gmail.com>
2023-10-22 18:33:33 +0530

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <div>
-        <Button colorScheme="whatsapp">Button</Button>
-      </div>
-    </>
+    <Grid
+      templateAreas={{
+        base: `'nav' 'main'`,
+        lg: ` "nav nav" "aside main" `,
+      }}
+    >
+      <GridItem h={"60px"} area={"nav"} bg={"green.400"}>
+        NavBar
+      </GridItem>
+      <Show above="lg">
+        <GridItem h={"100vh"} area={"aside"} bg={"yellow.300"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem  area={"main"} bg={"orange.300"}>
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
