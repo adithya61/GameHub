790e9d45ce3aafb4640a293dcd08edbd75cebdf3
adithya61 <zonic662@gmail.com>
2023-10-22 22:52:03 +0530

    implement dark mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Button, Flex, HStack, Image, useColorMode } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <HStack>
+    <HStack display={'Flex'}>
       <Image src={logo} boxSize={"60px"} />
+      <Button justifyContent={"flex-end"} onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
     </HStack>
   );
 };
